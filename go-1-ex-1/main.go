7661216fdacca01b69a27526f173a7f00b22c569
Jacob <110538047+Jsigi@users.noreply.github.com>
2024-11-28 15:35:45 +0100

    exercises-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var firstName string = "Jacob"
+var lastName string = "Sigrist"
+var dayOfBirth int = 24
+var monthOfBirth int = 5
+var yearOfBirth int = 2007
+var numberOfSiblings int = 2
+var heightInMeters float32 = 1.8
+var zodiacSign = '\u264A'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
