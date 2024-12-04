f2b63fd9ca5a81bf040b0db3f1fba43bd7e9b255
Jacob <110538047+Jsigi@users.noreply.github.com>
2024-12-04 08:42:11 +0100

    update-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,22 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Programming Essentials",
+		117: "Algorithm Design and Analysis",
+		346: "Mastering Go Development",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 104)
 	// TODO: add one
+	modules[666] = "System-Database"
 	// TODO: replace one
+	modules[346] = "Programierung der Database"
+
 	fmt.Println(modules)
 }
